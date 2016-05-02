6d929bcb28cc3c294b69a913d8356a9da5440f8b
Iris Caffin <iriscaffin@gmail.com>
2016-05-01 21:26:30 -0700

    Added a constructor

--- Grades/Grades/GradeBook.cs
+++ Grades/Grades/GradeBook.cs
@@ -8,6 +8,12 @@ namespace Grades
 {
     class GradeBook
     {
+        public GradeBook()
+        {
+
+        }
+
+
         public void AddGrade(float grade)
         {
             grades.Add(grade);   
